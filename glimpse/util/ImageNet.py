f590b51ca69e51161039c55b153c7f3a6310cd5a
GeorgeDittmar <georgedittmar@gmail.com>
2014-04-09 21:22:57 -0700

    Added ability to download and crop images from ImageNet as specified by Hintons group on using imagenet with Deep Neural Networks.

--- glimpse/util/ImageNet.py
+++ glimpse/util/ImageNet.py
@@ -35,29 +35,36 @@ class ImageNet():
     '''
     crop each image from imagenet to be 256x256 based on the images shortest side.
     '''            
-    def normalizeData(self,dir):
+    def normalizeCorpus(self,dir):
         
         for img in os.listdir(dir):
             print img
             # find the shortest side of the image then scale by that side
             tmp = Image.open(os.path.join(dir,img))
             width,height = tmp.size
-            normalized = gimage.ScaleAndCropImage(tmp, (256,256))
-            normalized.save(os.path.join(dir,img),"JPEG")
+            #normalized = gimage.ScaleAndCropImage(tmp, (256,256))
+            #normalized.save(os.path.join(dir,img),"JPEG")
             #scale the shortest side to 256 pixels then crop out an even 256 x 256 image
-            '''
+            
             if width <= height:
                 # figure out the height percent that we need to scale by to get 256 scaling ratio
-                hScale = 256/float(height)
+                hScale = 256/float(width)
                 newHeight = int(float(height)*float(hScale))
                 tmp = tmp.resize((256,newHeight),Image.ANTIALIAS)
-            
+                w,h = tmp.size
+                top = (h-256)/2
+                bottom = (h+256)/2
+                tmp = tmp.crop((0,top,w,bottom))
+                
             elif height <= width:
                 
-                wScale = float(256)/float(width)
+                wScale = float(256)/float(height)
                 newWidth = int(float(width)*float(wScale))
                 tmp = tmp.resize((newWidth,256),Image.ANTIALIAS)
-            '''
+                w,h = tmp.size
+                left = (w-256)/2
+                right = (w+256)/2
+                tmp = tmp.crop((left,0,right,h))
             #first check if shortest size is greater than 256 pixels
-            
+            tmp.save(os.path.join(dir,img),"JPEG")
             
